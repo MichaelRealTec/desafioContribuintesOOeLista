6b704d0ed89f62243e635c80688fac6bf6408b87
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-20 14:51:27 -0300

    desafio resolvido - DevSuperior

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,60 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.TaxPayer;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// git@github.com:MichaelRealTec/desafioContribuintesOOeLista.git
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		double salaryIncome;
+		double servicesIncome;
+		double capitalIncome;
+		double healthSpending;
+		double educationSpending;
+		
+		System.out.print("Quantos contribuintes você vai digitar? ");
+		n = sc.nextInt();
+	
+		System.out.println();
+		
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.printf("Digite os dados do %do contribuinte:\n",(i+1));
+			System.out.print("Renda anual com salário: ");
+			salaryIncome = sc.nextDouble();
+			System.out.print("Renda anual com prestação de serviço: ");
+			servicesIncome = sc.nextDouble();
+			System.out.print("Renda anual com ganho de capital: ");
+			capitalIncome = sc.nextDouble();
+			System.out.print("Gastos médicos: ");
+			healthSpending = sc.nextDouble();
+			System.out.print("Gastos educacionais: ");
+			educationSpending = sc.nextDouble();
+			
+			TaxPayer taxPayer = new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending);		
+			list.add(taxPayer);
+			System.out.println();
+		}
+		int i = 1;
+		for(TaxPayer taxPayer : list) {			
+			System.out.printf("Resumo do %do contribuinte: \n", i);
+			i = i + 1;
+			System.out.println(taxPayer);
+		}
+		
+		
+		sc.close();
 	}
 
 }
--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -1,5 +1,127 @@
 package entities;
 
 public class TaxPayer {
-
+	// Atributos
+	private double salaryIncome;
+	private double servicesIncome;
+	private double capitalIncome;
+	private double healthSpending;
+	private double educationSpending;
+	
+	// Métodos Getters e Setters
+	
+	public void setSalaryIncome(double salaryIncome) {
+		this.salaryIncome = salaryIncome;
+	}
+	
+	public double getSalaryIncome() {
+		return salaryIncome;
+	}
+	
+	public void setServicesIncome(double servicesIncome) {
+		this.servicesIncome = servicesIncome;
+	}
+	
+	public double getServicesIncome() {
+		return servicesIncome;
+	}
+	
+	public void setCapitalIncome(double capitalIncome) {
+		this.capitalIncome = capitalIncome;
+	}
+	
+	public double getCapitalIncome() {
+		return capitalIncome;
+	}
+	
+	public void setHealthSpending(double healthSpending) {
+		this.healthSpending = healthSpending;
+	}
+	
+	public double getHealthSpending() {
+		return healthSpending;
+	}
+	
+	public void setEducationSpending(double educationSpending) {
+		this.educationSpending = educationSpending;
+	}
+	public double getEducationSpending() {
+		return educationSpending;
+	}
+	
+	
+	// Método Construtor
+	public TaxPayer() {
+		
+	}
+	
+	public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending) {
+		this.salaryIncome = salaryIncome;
+		this.servicesIncome = servicesIncome;
+		this.capitalIncome = capitalIncome;
+		this.healthSpending = healthSpending;
+		this.educationSpending = educationSpending;
+	}
+	// Métodos
+	public double salaryTax() {
+		double salary = salaryIncome / 12;
+		double taxSalary = 0;
+		if(salary < 3000) {
+			//salary = salary;
+		} else if(salary < 5000) {
+			//salary -= (salary * 0.10);
+			taxSalary = salaryIncome * 0.10;			
+		} else if(salary >= 5000) {
+			//salary -= (salary * 0.20);
+			taxSalary = salaryIncome * 0.20;
+		}		
+		return taxSalary;
+		// 37800 + 8277,73 + 4000,00 = 50077,73
+	}
+	
+	public double servicesTax() {
+		double taxServicesIncome;
+		if(servicesIncome > 0) { 
+			taxServicesIncome = servicesIncome * 0.15;
+		} else {
+			taxServicesIncome = servicesIncome;
+		}
+		return taxServicesIncome;
+	}
+	
+	public double capitalTax() {
+		double capitalTax;
+		if(capitalIncome > 0) {
+			capitalTax = (capitalIncome * 0.20);
+		} else {
+			capitalTax = capitalIncome;
+		}
+		return capitalTax;
+	}
+	
+	public double grossTax() {
+		double grossTax = salaryTax() + servicesTax() + capitalTax();
+		return grossTax;
+	}
+	
+	public double taxRebate() {
+		double taxRebate;
+		if(grossTax() * 0.30 > (healthSpending  + educationSpending)) {
+			taxRebate = healthSpending + educationSpending;
+		} else {
+			taxRebate = grossTax() * 0.30;
+		}
+		return taxRebate;
+	}
+	
+	public double netTax() {
+		return grossTax() - taxRebate();
+	}
+	
+	public String toString() {
+		return "Imposto bruto total: "
+				+ String.format("%.2f%n",grossTax())
+				+ String.format("Abatimento: %.2f%n", taxRebate())
+				+ String.format("Imposto devido: %.2f%n", netTax());
+	}
 }
